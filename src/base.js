9b7636ff237a24be89502d1c71aabc63bc107da1
Gabor Szephalmi <szephalmig@gmail.com>
2019-02-05 18:40:07 +0000

    Add variables, initial commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+///////////an object that contains the selected elements from html
+
+export const elements = {
+  holidaysContainer: document.querySelector(".holidays__hotel-list"),
+  sortControlBtns: document.querySelectorAll(".sort-control__item"),
+  airportBtns: document.querySelectorAll("input"),
+  filterBtn: document.querySelector(".btn-filter"),
+  dateInputs: document.querySelectorAll("option")
+};

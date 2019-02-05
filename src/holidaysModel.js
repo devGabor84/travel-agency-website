b636907033649f1ce7634fd1483c856f7fd840f0
Gabor Szephalmi <szephalmig@gmail.com>
2019-02-05 18:37:20 +0000

    Add data about the holidays to be rendered

---
+++ src/holidaysModel.js
@@ -0,0 +1,116 @@
+export const holidays = [
+  {
+    name: "Adeje Hotel",
+    location: "Costa Adeje, Tenerife",
+    ratingValue: 5,
+    price: "£1,135.50",
+    priceValue: 1135.6,
+    imgPath: "./assets/hotel1.jpeg",
+    startDate: "3 July 2019",
+    date: "july",
+    duration: 7,
+    airport: "East Midlands",
+    airportCheck: "midlands",
+    adults: 2,
+    children: 2,
+
+    infants: 1,
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus asperiores dolores quo animi labore aliquam dolore fuga iusto, deserunt repudiandae quaerat quidem praesentium veritatis aliquid non similique? Labore, quam fugit! Excepturi debitis quia sequi quidem quasi veritatis quaerat sint fugiat!"
+  },
+  {
+    name: "Aguamarina Hotel",
+    location: "Costa Adeje, Tenerife",
+    ratingValue: 4,
+    price: "£499.99",
+    priceValue: 499.99,
+    imgPath: "./assets/hotel2.jpeg",
+    startDate: "5 May 2019",
+    date: "may",
+    duration: 7,
+    airport: "East Midlands",
+    airportCheck: "midlands",
+    adults: 2,
+    children: 2,
+
+    infants: 0,
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus asperiores dolores quo animi labore aliquam dolore fuga iusto, deserunt repudiandae quaerat quidem praesentium veritatis aliquid non similique? Labore, quam fugit! Excepturi debitis quia sequi quidem quasi veritatis quaerat sint fugiat!"
+  },
+  {
+    name: "Perada Hotel",
+    location: "Costa Brava, Spain",
+    ratingValue: 3,
+    price: "£510.70",
+    priceValue: 510.7,
+    imgPath: "./assets/hotel3.jpeg",
+    startDate: "12 July 2019",
+    date: "july",
+    duration: 7,
+    airport: "Manchester",
+    airportCheck: "manchester",
+    adults: 2,
+    children: 1,
+
+    infants: 0,
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus asperiores dolores quo animi labore aliquam dolore fuga iusto, deserunt repudiandae quaerat quidem praesentium veritatis aliquid non similique? Labore, quam fugit! Excepturi debitis quia sequi quidem quasi veritatis quaerat sint fugiat!"
+  },
+  {
+    name: "Laguna Park II",
+    location: "Costa Adeje, Tenerife",
+    ratingValue: 5,
+    price: "£966.80",
+    priceValue: 966.8,
+    imgPath: "./assets/hotel4.jpeg",
+    startDate: "18 May 2019",
+    date: "may",
+    duration: 7,
+    airport: "Liverpool",
+    airportCheck: "liverpool",
+    adults: 2,
+    children: 2,
+
+    infants: 0,
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus asperiores dolores quo animi labore aliquam dolore fuga iusto, deserunt repudiandae quaerat quidem praesentium veritatis aliquid non similique? Labore, quam fugit! Excepturi debitis quia sequi quidem quasi veritatis quaerat sint fugiat!"
+  },
+  {
+    name: "Holiday Resort",
+    location: "Kandooma, Maldives",
+    ratingValue: 3,
+    price: "£1,205.80",
+    priceValue: 1205.8,
+    imgPath: "./assets/hotel5.jpeg",
+    startDate: "21 July 2019",
+    date: "july",
+    duration: 7,
+    airport: "Manchester",
+    airportCheck: "manchester",
+    adults: 2,
+    children: 1,
+
+    infants: 0,
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus asperiores dolores quo animi labore aliquam dolore fuga iusto, deserunt repudiandae quaerat quidem praesentium veritatis aliquid non similique? Labore, quam fugit! Excepturi debitis quia sequi quidem quasi veritatis quaerat sint fugiat!"
+  },
+  {
+    name: "Resort Valle",
+    location: "Villasimius, Sardinia",
+    ratingValue: 4,
+    price: "£1,155.80",
+    priceValue: 696.8,
+    imgPath: "./assets/hotel6.jpeg",
+    startDate: "27 May 2019",
+    date: "may",
+    duration: 7,
+    airport: "Liverpool",
+    airportCheck: "liverpool",
+    adults: 2,
+    children: 2,
+
+    infants: 1,
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus asperiores dolores quo animi labore aliquam dolore fuga iusto, deserunt repudiandae quaerat quidem praesentium veritatis aliquid non similique? Labore, quam fugit! Excepturi debitis quia sequi quidem quasi veritatis quaerat sint fugiat!"
+  }
+];
